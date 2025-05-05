3d3bd5999c72ae933e60d6f907a0b47411bcbcf8
Lucas Faria <lucasalbfar@gmail.com>
2025-05-05 19:14:26 +0000

    Include prometheus and grafana

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 """Main route."""
 
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.api.endpoints.example import router as example_route
 from app.api.endpoints.load import router as load_router
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
 app.include_router(example_route)
 app.include_router(load_router, prefix="/load", tags=["Load Testing"])
 
